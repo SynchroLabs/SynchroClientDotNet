0d760b69c50ccbc9f2c4e762cfac5dfeba1f181c
BobDickinson <bob.dickinson@gmail.com>
2014-01-29 11:29:36 -0800

    WinPhone list view working!

--- MaaasClientWinPhone/Controls/WinPhoneListViewWrapper.cs
+++ MaaasClientWinPhone/Controls/WinPhoneListViewWrapper.cs
@@ -3,6 +3,7 @@ using Microsoft.Phone.Controls;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,11 +26,11 @@ namespace MaaasClientWinPhone.Controls
     class ListViewItem : Grid
     {
         CheckBox _check;
-        FrameworkElement _content;
+        WinPhoneControlWrapper _controlWrapper;
 
-        public ListViewItem(FrameworkElement content, ListViewSelectionMode selectionMode, bool targetingRequired) : base()
+        public ListViewItem(WinPhoneControlWrapper controlWrapper, ListViewSelectionMode selectionMode, bool targetingRequired) : base()
         {
-            _content = content;
+            _controlWrapper = controlWrapper;
 
             if ((selectionMode != ListViewSelectionMode.None) || (targetingRequired))
             {
@@ -62,13 +63,14 @@ namespace MaaasClientWinPhone.Controls
 
                 // Column one is where the listview contents goes...
                 //
-                Grid.SetColumn(_content, 1);
+                Grid.SetColumn(_controlWrapper.Control, 1);
             }
 
-            _content.VerticalAlignment = VerticalAlignment.Center;
-            this.Children.Add(_content);
+            _controlWrapper.Control.VerticalAlignment = VerticalAlignment.Center;
+            this.Children.Add(_controlWrapper.Control);
         }
 
+        public WinPhoneControlWrapper ControlWrapper { get { return _controlWrapper; } }
         public CheckBox CheckBox { get { return _check; } }
     }
 
@@ -81,16 +83,16 @@ namespace MaaasClientWinPhone.Controls
             base(parent, bindingContext)
         {
             Util.debug("Creating listview element");
-            LongListSelector listview = new LongListSelector();
-            this._control = listview;
+            LongListSelector listView = new LongListSelector();
+            this._control = listView;
 
-            listview.IsGroupingEnabled = false;
+            listView.IsGroupingEnabled = false;
 
-            listview.SelectionChanged += listview_SelectionChanged;
-            listview.ItemRealized += listview_ItemRealized;
-            listview.ItemUnrealized += listview_ItemUnrealized;
+            listView.SelectionChanged += listview_SelectionChanged;
+            listView.ItemRealized += listview_ItemRealized;
+            listView.ItemUnrealized += listview_ItemUnrealized;
 
-            applyFrameworkElementDefaults(listview);
+            applyFrameworkElementDefaults(listView);
 
             // Get selection mode - single (default) or multiple - no dynamic values (we don't need this changing during execution).
             if ((controlSpec["select"] != null) && ((string)controlSpec["select"] == "Multiple"))
@@ -101,6 +103,9 @@ namespace MaaasClientWinPhone.Controls
             {
                 _selectionMode = ListViewSelectionMode.None;
             }
+
+            ObservableCollection<ListViewItem> items = new ObservableCollection<ListViewItem>();
+            listView.ItemsSource = items;
 
             JObject bindingSpec = BindingHelper.GetCanonicalBindingSpec(controlSpec, "items", new string[] { "onItemClick" });
 
@@ -114,7 +119,6 @@ namespace MaaasClientWinPhone.Controls
             {
                 if (bindingSpec["items"] != null)
                 {
-                    /*
                     string itemSelector = (string)bindingSpec["item"];
                     if (itemSelector == null)
                     {
@@ -133,13 +137,10 @@ namespace MaaasClientWinPhone.Controls
                         (string)bindingSpec["items"],
                         () => getListViewContents(listView),
                         value => this.setListViewContents(listView, (JObject)controlSpec["itemTemplate"], GetValueBinding("items").BindingContext, itemSelector));
-
-                     */
                 }
 
                 if (bindingSpec["selection"] != null)
                 {
-                    /*
                     string selectionItem = (string)bindingSpec["selectionItem"];
                     if (selectionItem == null)
                     {
@@ -151,23 +152,163 @@ namespace MaaasClientWinPhone.Controls
                         (string)bindingSpec["selection"],
                         () => getListViewSelection(listView, selectionItem),
                         value => this.setListViewSelection(listView, selectionItem, (JToken)value));
-                
-                     */
+                }
+            }
+        }
+
+        public JToken getListViewContents(LongListSelector listbox)
+        {
+            Util.debug("Get listview contents - NOOP");
+            throw new NotImplementedException();
+        }
+
+        public void setListViewContents(LongListSelector listview, JObject itemTemplate, BindingContext bindingContext, string itemSelector)
+        {
+            Util.debug("Setting listview contents");
+
+            List<BindingContext> itemContexts = bindingContext.SelectEach(itemSelector);
+
+            Collection<ListViewItem> items = (Collection<ListViewItem>)listview.ItemsSource;
+
+            if (items.Count < itemContexts.Count)
+            {
+                // New items are added (to the end of the list)
+                //
+                for (int index = items.Count; index < itemContexts.Count; index++)
+                {
+                    WinPhoneControlWrapper controlWrapper = CreateControl(this, itemContexts[index], itemTemplate);
+                    items.Add(new ListViewItem(controlWrapper, _selectionMode, _targetingRequired));
+                }
+            }
+            else if (items.Count > itemContexts.Count)
+            {
+                // Items need to be removed (from the end of the list)
+                //
+                for (int index = items.Count; index > itemContexts.Count; index--)
+                {
+                    ListViewItem listViewItem = (ListViewItem)items[index - 1];
+
+                    // Unregister any bindings for this element or any descendants
+                    //
+                    listViewItem.ControlWrapper.Unregister();
+
+                    items.RemoveAt(index - 1);
                 }
             }
 
-            List<FrameworkElement> elements = new List<FrameworkElement>();
-            elements.Add(CreateItem("Item One"));
-            elements.Add(CreateItem("Item Two"));
-            elements.Add(CreateItem("Item Three"));
-            listview.ItemsSource = elements;
+            // This notification that the list backing this view has changed happens after the underlying bound values, and thus the list
+            // view items themselves, have been updated.  We need to maintain the selection state, but that is difficult as items may
+            // have "moved" list positions without any specific notification (other than this broad notification that the list itself changed).
+            //
+            // To address this, we get the "selection" binding for this list view, if any, and force a view update to reset the selection
+            // state from the view model whenever the list bound to the list view changes (and after we've processed any adds/removes above).
+            //
+            ValueBinding selectionBinding = GetValueBinding("selection");
+            if (selectionBinding != null)
+            {
+                selectionBinding.UpdateViewFromViewModel();
+            }
         }
 
-        FrameworkElement CreateItem(string content)
+        // To determine if an item should selected, get an item from the list, get the ElementMetaData.BindingContext.  Apply any
+        // selectionItem to the binding context, resolve that and compare it to the selection (selectionItem will always be provided
+        // here, and will default to "$data").
+        //
+        public JToken getListViewSelection(LongListSelector listview, string selectionItem)
         {
-            TextBlock textBlock = new TextBlock();
-            textBlock.Text = content;
-            return new ListViewItem(textBlock, _selectionMode, _targetingRequired);
+            if (_selectionMode == ListViewSelectionMode.Multiple)
+            {
+                List<ControlWrapper> selected = new List<ControlWrapper>();
+                foreach (ListViewItem listItem in listview.ItemsSource)
+                {
+                    if (listItem.CheckBox.IsChecked == true)
+                    {
+                        selected.Add(listItem.ControlWrapper);
+                    }
+                }
+
+                return new JArray(
+                    from ControlWrapper controlWrapper in selected
+                    select controlWrapper.BindingContext.Select(selectionItem).GetValue()
+                );
+            }
+            else if (_selectionMode == ListViewSelectionMode.Single)
+            {
+                ControlWrapper selected = null;
+                foreach (ListViewItem listItem in listview.ItemsSource)
+                {
+                    if (listItem.CheckBox.IsChecked == true)
+                    {
+                        selected = listItem.ControlWrapper;
+                        break;
+                    }
+                }
+
+                if (selected != null)
+                {
+                    // We need to clone the item so we don't destroy the original link to the item in the list (since the
+                    // item we're getting in SelectedItem is the list item and we're putting it into the selection binding).
+                    //     
+                    return selected.BindingContext.Select(selectionItem).GetValue().DeepClone();
+                }
+                return new JValue(false); // This is a "null" selection
+            }
+
+            return null;
+        }
+
+        // This gets triggered when selection changes come in from the server (including when the selection is initially set),
+        // and it also gets triggered when the list itself changes (including when the list contents are intially set).  So 
+        // in the initial list/selection set case, this gets called twice.  On subsequent updates it's possible that this will
+        // be triggered by either a list change or a selection change from the server, or both.  There is no easy way currerntly
+        // to detect the "both" case (without exposing a lot more information here).  We're going to go ahead and live with the
+        // multiple calls.  It shouldn't hurt anything (they should produce the same result), it's just slightly inefficient.
+        // 
+        public void setListViewSelection(LongListSelector listview, string selectionItem, JToken selection)
+        {
+            if (_selectionMode == ListViewSelectionMode.Multiple)
+            {
+                foreach (ListViewItem listItem in listview.ItemsSource)
+                {
+                    bool selected = false;
+
+                    if (selection is JArray)
+                    {
+                        JArray array = selection as JArray;
+                        foreach (JToken item in array.Children())
+                        {
+                            if (JToken.DeepEquals(item, listItem.ControlWrapper.BindingContext.Select(selectionItem).GetValue()))
+                            {
+                                selected = true;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (JToken.DeepEquals(selection, listItem.ControlWrapper.BindingContext.Select(selectionItem).GetValue()))
+                        {
+                            selected = true;
+                        }
+                    }
+
+                    listItem.CheckBox.IsChecked = selected;
+                }
+            }
+            else if (_selectionMode == ListViewSelectionMode.Single)
+            {
+                foreach (ListViewItem listItem in listview.ItemsSource)
+                {
+                    bool selected = false;
+
+                    if (JToken.DeepEquals(selection, listItem.ControlWrapper.BindingContext.Select(selectionItem).GetValue()))
+                    {
+                        selected = true;
+                    }
+
+                    listItem.CheckBox.IsChecked = selected;
+                }
+            }
         }
 
         void listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -183,16 +324,20 @@ namespace MaaasClientWinPhone.Controls
                     if (_selectionMode == ListViewSelectionMode.Multiple)
                     {
                         item.CheckBox.IsChecked = !item.CheckBox.IsChecked;
+                        updateValueBindingForAttribute("selection");
                     }
                     else if (_selectionMode == ListViewSelectionMode.Single)
                     {
                         if (item.CheckBox.IsChecked == false)
                         {
+                            bool selectionChanged = false;
+
                             foreach(ListViewItem listItem in listview.ItemsSource)
                             {
                                 if (listItem == item)
                                 {
                                     listItem.CheckBox.IsChecked = true;
+                                    selectionChanged = true;
                                 }
                                 else
                                 {
@@ -201,6 +346,11 @@ namespace MaaasClientWinPhone.Controls
                                         listItem.CheckBox.IsChecked = false;
                                     }
                                 }
+                            }
+
+                            if (selectionChanged)
+                            {
+                                updateValueBindingForAttribute("selection");
                             }
                         }
                     }
@@ -213,14 +363,11 @@ namespace MaaasClientWinPhone.Controls
 
                     // The item click command handler resolves its tokens relative to the item clicked (not the list view).
                     //
-                    // !!! Store the wrapper for the item in the ListViewItem and get it back out here...
-                    /*
-                    ControlWrapper wrapper = this.getChildControlWrapper((FrameworkElement)e.ClickedItem);
+                    ControlWrapper wrapper = item.ControlWrapper;
                     if (wrapper != null)
                     {
                         StateManager.processCommand(command.Command, command.GetResolvedParameters(wrapper.BindingContext));
                     }
-                     */
                 }
 
                 listview.SelectedItem = null;
@@ -229,17 +376,20 @@ namespace MaaasClientWinPhone.Controls
 
         void listview_ItemRealized(object sender, ItemRealizationEventArgs e)
         {
-            Util.debug("ListView ItemRealized");
             if (e.ItemKind == LongListSelectorItemKind.Item)
             {
-                // e.Container is ContentPresenter
-                // e.Container.Content - this is whatever was in the list...
+                ListViewItem listViewItem = (ListViewItem)e.Container.Content;
+                Util.debug("ListView ItemRealized: " + listViewItem.ControlWrapper);
             }
         }
 
         void listview_ItemUnrealized(object sender, ItemRealizationEventArgs e)
         {
-            Util.debug("ListView ItemUnrealized");
+            if (e.ItemKind == LongListSelectorItemKind.Item)
+            {
+                ListViewItem listViewItem = (ListViewItem)e.Container.Content;
+                Util.debug("ListView ItemUnrealized: " + listViewItem.ControlWrapper);
+            }
         }
     }
 }
