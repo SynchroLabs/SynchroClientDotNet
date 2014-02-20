ef3718a0ec48170e7f8dc39477f012a2323200e5
BobDickinson <bob.dickinson@gmail.com>
2014-02-20 13:21:22 -0800

    iOS toolbar pretty much sorted (including adding IconBeast toolbar icon library).

--- MaaasClientIOS/Controls/iOSToolBarWrapper.cs
+++ MaaasClientIOS/Controls/iOSToolBarWrapper.cs
@@ -17,18 +17,48 @@ namespace MaaasClientIOS.Controls
         public iOSToolBarWrapper(ControlWrapper parent, BindingContext bindingContext, JObject controlSpec) :
             base(parent, bindingContext)
         {
-            Util.debug("Creating tool bar button element with text of: " + controlSpec["text"]);
+            Util.debug("Creating tool bar button element");
 
-            UIBarButtonItem buttonItem = new UIBarButtonItem("", UIBarButtonItemStyle.Plain, buttonItem_Clicked);
-            processElementProperty((string)controlSpec["text"], value => buttonItem.Title = ToString(value));
+            UIBarButtonItem buttonItem = null;
+
+            // Note: Because the navBar button does not coerce/stlye images, it is recommended that you use either a systemItem or text-only
+            //       button element on the navBar (especially important for correct styling across different versions of iOS).
+            //
+
+            if (controlSpec["systemItem"] != null)
+            {
+                // System items: 
+                //
+                //     Done, Cancel, Edit, Save, Add, Compose, Reply, Action, Organize, Bookmarks, Search, Refresh, Stop, 
+                //     Camera, Trash, Play, Pause, Rewind, FastForward, Undo, Redo, PageCurl
+                //
+                //     https://developer.apple.com/library/ios/documentation/uikit/reference/UIBarButtonItem_Class/Reference/Reference.html
+                //
+                UIBarButtonSystemItem item = (UIBarButtonSystemItem)typeof(UIBarButtonSystemItem).GetField((string)controlSpec["systemItem"]).GetValue(null);
+                buttonItem = new UIBarButtonItem(item, buttonItem_Clicked);
+            }
+            else
+            {
+                // Custom items, can specify text, icon, or both
+                //
+                buttonItem = new UIBarButtonItem("", UIBarButtonItemStyle.Plain, buttonItem_Clicked);
+                processElementProperty((string)controlSpec["text"], value => buttonItem.Title = ToString(value));
+                processElementProperty((string)controlSpec["icon"], value => buttonItem.Image = UIImage.FromBundle("icons/blue/" + ToString(value)));
+            }
+            
             processElementProperty((string)controlSpec["enabled"], value => buttonItem.Enabled = ToBoolean(value));
 
             if ((string)controlSpec["control"] == "navBar.button")
             {
+                // When image and text specified, uses image.  Image is placed on button surface verbatim (no color coersion).
+                //
                 _pageView.SetNavBarButton(buttonItem);
             }
             else // toolBar.button
             {
+                // Can use image, text, or both, and toolbar shows what was provided (including image+text).  Toolbar coerces colors
+                // and handles disabled state (for example, on iOS 6, icons/text show up as white when enabled and gray when disabled).
+                //
                 _pageView.AddToolbarButton(buttonItem);
             }
 
